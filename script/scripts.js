998154e00c957d36377386b27c478d6c1e8db4fc
NataliaReiis <nataliareis208@gmail.com>
2024-12-02 13:25:48 -0300

    :tada:

---
+++ script/scripts.js
@@ -0,0 +1,71 @@
+import selecionaCotacao from "./imprimeCotacao.js";
+
+function geraHorario() {
+  let data = new Date();
+  let horario =
+    data.getHours() + ":" + data.getMinutes() + ":" + data.getSeconds();
+  console.log(horario);
+  return horario;
+}
+
+function adicionarDados(grafico, legenda, dados) {
+  grafico.data.labels.push(legenda);
+  grafico.data.datasets.forEach((dataset) => {
+    dataset.data.push(dados);
+  });
+  grafico.update();
+}
+
+//Grafico Dolar
+
+const graficoDolar = document.getElementById("graficoDolar");
+const graficoParaDolar = new Chart(graficoDolar, {
+  type: "line",
+  data: {
+    labels: [],
+    datasets: [
+      {
+        label: "Dólar",
+        data: [],
+        borderWidth: 1,
+      },
+    ],
+  },
+});
+
+let workerDolar = new Worker("./script/workers/workerDolar.js");
+workerDolar.postMessage("usd");
+
+workerDolar.addEventListener("message", (event) => {
+  let tempo = geraHorario();
+  let valor = event.data.ask;
+  adicionarDados(graficoParaDolar, tempo, valor);
+  selecionaCotacao("dolar", valor);
+});
+
+//Grafico Iene
+
+const graficoIene = document.getElementById("graficoIene");
+const graficoParaIene = new Chart(graficoIene, {
+  type: "line",
+  data: {
+    labels: [],
+    datasets: [
+      {
+        label: "Iene",
+        data: [],
+        borderWidth: 1,
+      },
+    ],
+  },
+});
+
+let workerIene = new Worker("./script/workers/workerIene.js");
+workerIene.postMessage("iene");
+
+workerIene.addEventListener("message", (event) => {
+  let tempo = geraHorario();
+  let valor = event.data.ask;
+  adicionarDados(graficoParaIene, tempo, valor);
+  selecionaCotacao("iene", valor);
+});
